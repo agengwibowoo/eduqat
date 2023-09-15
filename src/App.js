73926301ca7b15936d11eb4384e734a4334d8d20
Ageng Wibowo <ageng.wibowo@dataon.com>
2023-09-16 04:50:20 +0700

    enc: init chakra ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider, theme } from "@chakra-ui/react";
 import Routing from "./Routing";
 import Layout from "./components/layout/Layout";
 import Theme from "./styles/Theme";
@@ -7,14 +8,16 @@ import GlobalStyles from "./styles/Global";
 
 function App() {
     return (
-        <Theme>
-            <GlobalStyles />
-            <Router>
-                <Layout>
-                    <Routing />
-                </Layout>
-            </Router>
-        </Theme>
+        <ChakraProvider theme={theme}>
+            <Theme>
+                <GlobalStyles />
+                <Router>
+                    <Layout>
+                        <Routing />
+                    </Layout>
+                </Router>
+            </Theme>
+        </ChakraProvider>
     );
 }
 
